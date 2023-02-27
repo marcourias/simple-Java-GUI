b5a63c5d8f1ce0d5615a8c5f0e8967894527f814
Marco U <91865284+marcourias@users.noreply.github.com>
2023-02-26 21:37:26 -0600

    Update Menu.java

--- src/pizzeria/Menu.java
+++ src/pizzeria/Menu.java
@@ -1,8 +1,7 @@
 package pizzeria;
 
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
+ This is a simple JAVA GUI done with NetBeans interface. On it I demonstrate basic Swing components such as JButtons and JLabels.
  */
 
 /**
